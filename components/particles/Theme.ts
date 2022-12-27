f5cede2a03ee07afbcd0ed56728368dbb0235108
YeonghunKO <yhko1988@gmail.com>
2022-12-27 15:41:03 +0900

    style : primary, secondary, tertiary 추가
    - Loader, modal button 색깔을 위해서

--- components/particles/Theme.ts
+++ components/particles/Theme.ts
@@ -16,7 +16,18 @@ const Theme: DefaultTheme = {
   white: '#ffffff',
   offWhite: '#F0F0F0',
 
+  grey: '#a6acb0',
+  grey100: '#f0f1f2',
+
   primary: '#eee',
+  primary300: '#f4f4f4',
+  primary500: '#e6e6e6',
+  primary700: '#c7c7c7',
+  primary900: '#a8a8a8',
+
+  secondary: '#54d75f',
+
+  tertiary: '#de4536',
 };
 
 export default Theme;
--- style.d.ts
+++ style.d.ts
@@ -8,6 +8,17 @@ declare module 'styled-components' {
     white: '#ffffff';
     offWhite: '#F0F0F0';
 
+    grey: '#a6acb0';
+    grey100: '#f0f1f2';
+
     primary: '#eee';
+    primary300: '#f4f4f4';
+    primary500: '#e6e6e6';
+    primary700: '#c7c7c7';
+    primary900: '#a8a8a8';
+
+    tertiary: '#de4536';
+
+    secondary: '#54d75f';
   }
 }
