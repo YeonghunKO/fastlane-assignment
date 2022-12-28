ea67854b28a6d555013a5628677b8aa6c658f821
YeonghunKO <yhko1988@gmail.com>
2022-12-28 16:29:00 +0900

    feat : IssueList 페이지 완성

--- components/IssueList/Page/IssueList.styles.ts
+++ components/IssueList/Page/IssueList.styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const;
+export const Container = styled.main`
+  ${({ theme }) => theme.flexColumn};
+  background-color: ${({ theme }) => theme.backgroundColor};
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const LoaderContainer = styled.footer`
+  ${({ theme }) => theme.flexCenter};
+  width: 100%;
+  height: 3rem;
+`;
--- components/IssueList/Page/IssueList.tsx
+++ components/IssueList/Page/IssueList.tsx
@@ -1,16 +1,44 @@
-import { gitHubIssueService } from 'NetWork/GitHubIssueService';
-import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import React, { useEffect, useState } from 'react';
+
+import Theme from '@components/particles/Theme';
+
+import * as S from './IssueList.styles';
+import useInfiniteQueryScroll from 'hooks/useInfiniteQueryScroll';
+import Modal from '@components/common/Molecules/Modal';
+import { HTTP_ERROR } from 'constant/httpError';
+import Header from '../Atoms/Header';
+import { BeatLoader } from 'react-spinners';
+import IssuesContainer from '../Molecules/IssuesContainer';
+import PageToTopButton from '@components/common/Atoms/PageToTopButton';
 
 const IssueList = () => {
-  useEffect(() => {
-    async function getListByPage(page: number) {
-      const issues = await gitHubIssueService.getIssuesByPage(page);
-      console.log(issues);
-    }
+  const { ref, inView } = useInView();
+  const { data, error, fetchNextPage, hasNextPage, status } =
+    useInfiniteQueryScroll();
 
-    getListByPage(2);
-  }, []);
-  return <div>issueList</div>;
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
+  if (status === 'error') {
+    return <Modal message={HTTP_ERROR(error.message)} />;
+  }
+
+  return (
+    <S.Container>
+      <Header owner="facebook" repo="create-react-app" />
+      <IssuesContainer data={data?.pages} />
+      {hasNextPage && (
+        <S.LoaderContainer ref={ref}>
+          <BeatLoader size="1.5rem" color={Theme.tertiary} loading={true} />
+        </S.LoaderContainer>
+      )}
+      <PageToTopButton />
+    </S.Container>
+  );
 };
 
 export default IssueList;
