7d7fbf7d49c0d307021f31fd4666acf1d8a0aec8
YeonghunKO <yhko1988@gmail.com>
2022-12-28 16:28:00 +0900

    feat : 위로가기 버튼 theme에 따라서 색깔 바뀜

--- components/common/Atoms/PageToTopButton/PageToTopButton.styles.ts
+++ components/common/Atoms/PageToTopButton/PageToTopButton.styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.boxBackground};
+  color: ${({ theme }) => theme.title};
   position: fixed;
   bottom: 1rem;
   right: 1rem;
