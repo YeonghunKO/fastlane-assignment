690d551f0d103f00149bfe28f4194a3111e17e04
YeonghunKO <yhko1988@gmail.com>
2022-12-28 16:27:20 +0900

    feat : dark mode 전역화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,37 @@
 import type { AppProps } from 'next/app';
+
+import { useState } from 'react';
+
 import { ThemeProvider } from 'styled-components';
 import Theme, { DarkTheme } from '@components/particles/Theme';
 import GlobalStyle from '@components/particles/GlobalStyles';
+
 import {
   QueryClient,
   QueryClientProvider,
   Hydrate,
 } from '@tanstack/react-query';
+
+import LightDarkModeSwitch from '@components/common/Molecules/LightDarkModeSwitch';
+
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+import { useAtomValue } from 'jotai';
+import { isDarkModeState } from 'store/isDarkModeAtoms';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+  const isDarkMode = useAtomValue(isDarkModeState);
 
   return (
-    <ThemeProvider theme={Theme}>
+    <ThemeProvider theme={isDarkMode ? DarkTheme : Theme}>
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <ReactQueryDevtools initialIsOpen={true} />
-          <Component {...pageProps} />
+          <>
+            <LightDarkModeSwitch />
+            <Component {...pageProps} />
+          </>
         </Hydrate>
       </QueryClientProvider>
     </ThemeProvider>
---
+++ store/isDarkModeAtoms.ts
@@ -0,0 +1,3 @@
+import { atom } from 'jotai';
+
+export const isDarkModeState = atom(false);
